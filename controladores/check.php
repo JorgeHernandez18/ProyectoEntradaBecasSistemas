8ebf6be5ca6cb46ab1b7ff43e299c7b6e497f7cf
Biblioteca-Eduardo-Cote-Lamus <145394587+Biblioteca-Eduardo-Cote-Lamus@users.noreply.github.com>
2024-10-04 12:41:32 -0500

    Update check.php
    aumentar segurad contra inyeccion SQL

--- controladores/check.php
+++ controladores/check.php
@@ -6,15 +6,25 @@ include "../modelo/conexion.php"; // Incluye el archivo de conexión a la base d
 if (isset($_POST['usuario']) && isset($_POST['password'])) {
     // Verifica que se hayan enviado los campos 'usuario' y 'password' a través de POST.
 
-    // Consulta a la base de datos para verificar las credenciales del usuario.
-    $resultado = $conexion->query(
-        "SELECT * FROM admin WHERE usuario='" . $_POST['usuario'] . "' AND password='" . sha1($_POST['password']) . "'"
-    ) or die($conexion->error); // Ejecuta la consulta y muestra el error en caso de fallo.
-
-    // Verifica si la consulta devolvió algún resultado (usuario y contraseña correctos).
-    if (mysqli_num_rows($resultado) > 0) {
+    // Prepara la consulta SQL para evitar inyección SQL
+    $stmt = $conexion->prepare("SELECT * FROM admin WHERE usuario = ? AND password = ?");
+    
+    // Genera el hash de la contraseña
+    $passwordHash = sha1($_POST['password']);
+    
+    // Vincula los parámetros a la consulta
+    $stmt->bind_param("ss", $_POST['usuario'], $passwordHash);
+    
+    // Ejecuta la consulta
+    $stmt->execute();
+    
+    // Obtiene el resultado
+    $resultado = $stmt->get_result();
+    
+    // Verifica si la consulta devolvió algún resultado (usuario y contraseña correctos)
+    if ($resultado->num_rows > 0) {
         // Si hay resultados, obtiene la fila del usuario.
-        $datos_usuario = mysqli_fetch_row($resultado);
+        $datos_usuario = $resultado->fetch_row();
         $nombre = $datos_usuario[1]; // Nombre del usuario (suponiendo que es el segundo campo en la fila).
         $id_usuario = $datos_usuario[0]; // ID del usuario (suponiendo que es el primer campo en la fila).
         $nivel = $datos_usuario[3]; // Nivel del usuario (suponiendo que es el cuarto campo en la fila).
@@ -32,6 +42,9 @@ if (isset($_POST['usuario']) && isset($_POST['password'])) {
         // Si no hay resultados, redirige al usuario a la página de inicio de sesión con un mensaje de error.
         header("location: ../vistas/formularios/index.php?error=Credenciales incorrectas");
     }
+
+    // Cierra la declaración
+    $stmt->close();
 } else {
     // Si los campos 'usuario' o 'password' no están definidos, redirige al usuario a la página de inicio de sesión.
     header("location: ../vistas/formularios/index.php");
